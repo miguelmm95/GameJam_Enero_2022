03afe8a88573f4de77534f2a4751404d7ca1a1d1
miguelmm95 <79021700+miguelmm95@users.noreply.github.com>
2022-01-29 17:24:41 +0100

    BatEnemie

---
+++ Scripts/Enemies/Bat/lookAtPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtPlayer : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+    }
+}
